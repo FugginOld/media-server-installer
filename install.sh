80cfe600ee729a151bd69d1ada019bdadd83397e
Fuggin <121835359+FugginOld@users.noreply.github.com>
2026-03-06 08:51:59 -0500

    Refactor install.sh for better readability and updates
    Updated the install script to improve functionality and structure.

--- install.sh
+++ install.sh
@@ -1,23 +1,73 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-REPO="https://github.com/FugginOld/media-server-installer"
+REPO_URL="https://github.com/FugginOld/media-server-installer.git"
 INSTALL_DIR="/opt/media-server-installer"
 
-echo "Downloading Media Stack Installer..."
+echo "-----------------------------------------"
+echo " Media Server Installer Bootstrap"
+echo "-----------------------------------------"
 
-git clone $REPO $INSTALL_DIR
+########################################
+# Root check
+########################################
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root."
+  exit 1
+fi
+
+########################################
+# Install base dependencies
+########################################
+
+echo "Installing dependencies..."
+
+apt update
+
+apt install -y \
+git \
+curl \
+wget \
+whiptail \
+docker.io \
+docker-compose \
+jq \
+pciutils
+
+########################################
+# Enable docker
+########################################
+
+systemctl enable docker
+systemctl start docker
+
+########################################
+# Clone or update repository
+########################################
+
+if [ -d "$INSTALL_DIR/.git" ]; then
+
+  echo "Updating existing installer..."
+
+  cd $INSTALL_DIR
+  git pull
+
+else
+
+  echo "Downloading Media Stack Installer..."
+
+  git clone $REPO_URL $INSTALL_DIR
+
+fi
+
+########################################
+# Launch installer
+########################################
 
 cd $INSTALL_DIR
 
 chmod +x installer.sh
-chmod +x media-stack
 
-./installer.sh
-
-cp media-stack /usr/local/bin/media-stack
-
-echo ""
-echo "Media Stack installed successfully."
-echo "Use the command: media-stack"
+bash installer.sh
